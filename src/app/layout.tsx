0d2a37fa541f10b1ffbe6d20ac1d2e876fdb6f4b
matheus <matheus.felipe.19rt@gmail.com>
2024-08-25 17:22:34 -0300

    feat: Add & use Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 import { GlobalContextProvider } from "../components/GlobalContext"
 import "./globals.css"
 
@@ -7,9 +8,14 @@ export const metadata: Metadata = {
 	description: "Next.js Weather App",
 }
 
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={inter.className}>
 			<body>
 				<GlobalContextProvider>
 					<main>{children}</main>
