bf239af20936ec9d2549250b38f700133a2d268b
matheus <matheus.felipe.19rt@gmail.com>
2024-10-09 23:18:18 -0300

    chore: Update npm dependencies & Vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,15 @@
+import react from "@vitejs/plugin-react"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
-		include: ["./tests/**/*.{test,spec}.{js,ts}"],
+		include: ["./tests/**/*.{test,spec}.{js,ts,jsx,tsx}"],
 		exclude: ["node_modules", "tests/e2e", "dist"],
+		setupFiles: "./tests/setup.ts",
+		globals: true,
+		environment: "jsdom",
+		css: true,
 		coverage: {
 			provider: "v8",
 			include: ["src/**/*"],
